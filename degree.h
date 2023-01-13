4b2847f0ddbe80a1fcec739d91c456e6bcf2c582
Hywel (Howel) <92697786+HywelFrancis@users.noreply.github.com>
2023-01-12 20:27:20 -0500

    Add files via upload
    Define an enumerated data type DegreeProgram for the degree programs containing the data type values SECURITY, NETWORK, and SOFTWARE.

---
+++ degree.h
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Class Data System
+//
+//  Created by HYWEL-FRANCIS on 1/12/23.
+//
+//C. Define an enumerated data type DegreeProgram for the degree programs containing the datat type values SECURITY, NETWORK, and SOFTWARE
+#pragma once
+enum DegreeProgram {
+    SECURITY,
+    NETWORK,
+    SOFTWARE
+};
+
+
